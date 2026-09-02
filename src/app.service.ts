17e6d4d994589fb4b443416b3146313e35feead6
shama <shama.dmitry@gmail.com>
2026-09-02 11:47:11 +0300

    feat: add .env.prod to gitignore, update docker-compose for production, and enhance AppService to include NODE_ENV

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): { message: string } {
+  getHello(): { message: string; mode?: string } {
     return {
       message: 'Hello World!',
+      mode: process.env.NODE_ENV,
     };
   }
 }
